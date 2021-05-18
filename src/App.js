13ecfe17932b1198657cd9af61f12703d305ba2f
Phú Phan Văn <phudev95@gmail.com>
2021-05-18 16:38:39 +0700

    Just a demo from codesandbox

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { RaceRanking } from "./components/RaceRanking";
 function App() {
   return (
     <div className="app">
-      <h1 className="title">Race Ranking with React</h1>
+      <h1 className="title">Race Ranking with React 2</h1>
       <RaceRanking />
     </div>
   );
