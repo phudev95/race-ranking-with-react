cf3567718bf408ffe44550593b5e5b5af0c0bed9
Phú Phan Văn <phudev95@gmail.com>
2021-05-18 16:48:54 +0700

    Update style

--- src/components/RaceRanking/components/UserInformation/index.jsx
+++ src/components/RaceRanking/components/UserInformation/index.jsx
@@ -1,15 +1,20 @@
 import { memo } from "react";
 import { LAP_LENGTH } from "../../utils/constant";
 
-const UserInformationWithoutMemo = ({
-  raceRankingUser
-}) => {
+const UserInformationWithoutMemo = ({ raceRankingUser }) => {
   return (
     <div className="user-information" key={raceRankingUser.id}>
-      <img alt="" src={raceRankingUser.image}/>
+      <img alt="" src={raceRankingUser.image} />
       <label>{raceRankingUser.fullName}</label>
-      <meter value={raceRankingUser.progress % LAP_LENGTH} min="0" max={LAP_LENGTH} />
-      <i className="km">{raceRankingUser.progress.toFixed(2)} km (Round {Math.ceil(raceRankingUser.progress / LAP_LENGTH)})</i>
+      <meter
+        value={raceRankingUser.progress % LAP_LENGTH}
+        min="0"
+        max={LAP_LENGTH}
+      />
+      <i className="km">
+        {raceRankingUser.progress.toFixed(2)} km (Round{" "}
+        {Math.floor(raceRankingUser.progress / LAP_LENGTH) + 1})
+      </i>
     </div>
   );
 };
